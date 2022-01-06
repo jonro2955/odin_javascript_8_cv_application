e2b89ac339114dad9fa405e46a7eb39529b44686
Jonathan Ro <jonro.2955@gmail.com>
2022-01-05 18:50:35 -0800

    Set up photo url toggle form

--- src/components/level-3-Resume/Photo.js
+++ src/components/level-3-Resume/Photo.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
 class Photo extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      editPhoto: false,
+      photoURL:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU',
+    };
+  }
+
+  handlePhotoChange = (e) => {
+    this.setState({
+      photoURL: e.target.value,
+    });
+  };
+
+  toggleEditPhoto = () => {
+    this.setState((prevState) => ({
+      editPhoto: !prevState.editPhoto,
+    }));
+  };
 
   render() {
+    const { editPhoto, photoURL } = this.state;
     return (
-      <div id='Photo'>
+      <div id='Photo' onClick={this.toggleEditPhoto}>
         <p id='photoLabel'>Photo</p>
+        {editPhoto && (
+          <form
+            id='photoEditForm'
+            className='photo-edit-form'
+            onClick={this.toggleEditPhoto}
+          >
+            <label htmlFor='photoEditForm'>Photo URL</label>
+            <input id='photoURLInput' type='text' onChange={this.handlePhotoChange} />
+            <button type='submit'>Submit</button>
+          </form>
+        )}
       </div>
     );
   }
